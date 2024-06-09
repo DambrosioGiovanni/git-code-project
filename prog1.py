4b355e3ec3c078478b9dccf15e7fb9a099b4f7d3
Giovanni <giovanni.dambrosio@studenti.univr.it>
2024-06-09 16:57:15 +0200

    first DF adjusted

--- prog1.py
+++ prog1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 
+# combine the different csv files into 1 DataFrame
+
 directory = r'C:\Users\99gio\OneDrive\Desktop\prove python\dati 2000-2022'
 
 dataframes = []
@@ -12,4 +14,15 @@ for filename in os.listdir(directory):
 
 combined_df = pd.concat(dataframes, ignore_index=True)
 
-print(combined_df.head())
+#starting to adjust the dataframe by viewing and selecting the columns and raws i consider not relevant
+
+print(combined_df.columns)
+
+unique_stages = combined_df['Stage'].unique()
+print(unique_stages)
+
+combined_df = combined_df[combined_df['Stage'] != 'Qualifying']
+print(combined_df['Stage'].unique())
+
+combined_df = combined_df.drop(columns=['Stage','Round','Group','Date', 'Comments', '∑FT','ET','P'])
+print(combined_df)
