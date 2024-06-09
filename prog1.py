5b1fe98ce0050903a97cb38c95ca48e75c968d30
Giovanni <giovanni.dambrosio@studenti.univr.it>
2024-06-09 12:36:45 +0200

    Dataframe combined

--- prog1.py
+++ prog1.py
@@ -1,6 +1,15 @@
-a=4
-a
-b=a*2
-b
-c=b+a
-c
+import pandas as pd
+import os
+
+directory = r'C:\Users\99gio\OneDrive\Desktop\prove python\dati 2000-2022'
+
+dataframes = []
+
+for filename in os.listdir(directory):
+    filepath = os.path.join(directory, filename)
+    df = pd.read_csv(filepath)
+    dataframes.append(df)
+
+combined_df = pd.concat(dataframes, ignore_index=True)
+
+print(combined_df.head())
